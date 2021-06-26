8329187ce577e0c2199956739654cfb0c054a78c
TB <thaibang1983hn@gmail.com>
2021-06-26 12:08:42 +0200

    about page style

--- src/Components/AboutSkillsSection.js
+++ src/Components/AboutSkillsSection.js
@@ -4,12 +4,12 @@ function SkillsSection({ skill, progress, width }) {
   return (
     <div className="SkillsSection">
       <div className="skills-container">
-        <div className="skill-title">{skill}</div>
+        <h3 className="skill-title">{skill}</h3>
         <div className="skill-bar">
           <p className="skill-text">{progress}</p>
           <div className="skill-progress">
             <div className="progress">
-              <div className="inner-pregress" style={{ width: width }}></div>
+              <div className="inner-progress" style={{ width: width }}></div>
             </div>
           </div>
         </div>
