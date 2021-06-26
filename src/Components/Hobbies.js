0c740090cd404b80d3eec186c0382d4f64885ee2
TB <thaibang1983hn@gmail.com>
2021-06-26 19:38:27 +0200

    hobbies page

--- src/Components/Hobbies.js
+++ src/Components/Hobbies.js
@@ -1,13 +1,68 @@
-import React from "react";
+import codewars from "../img/hobbies/codewars.jpeg";
+import sciencefiction from "../img/hobbies/sciencefiction.png";
+import voyages from "../img/hobbies/voyages.png";
+import music from "../img/hobbies/music.jpeg";
+import football from "../img/hobbies/football.png";
+import badminton from "../img/hobbies/badminton.jpeg";
+import chess from "../img/hobbies/chess.png";
 
-function Hobbies() {
-  return (
-    <div>
-      <div>
-        <div></div>
-      </div>
-    </div>
-  );
-}
+const Hobbies = [
+  {
+    id: 1,
+    image: codewars,
+    title: "The website to learn code",
+    paragraph:
+      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum  Lorem ipsum Lorem ipsum Lorem ",
+    link: "https://www.codewars.com/users/quanaotat",
+  },
+  {
+    id: 2,
+    image: sciencefiction,
+    title: "The novel of science-fiction",
+    paragraph:
+      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+    link: "https://miscifi.com",
+  },
+  {
+    id: 3,
+    image: voyages,
+    title: "The trip to Maroc",
+    paragraph:
+      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+    link: "https://photos.google.com/sharing",
+  },
+  {
+    id: 4,
+    image: music,
+    title: "The music that I hear everyday",
+    paragraph:
+      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+    link: "https://www.youtube.com/channel/UC3Ab4R2NthIyqLB0Kt3xC1g/videos",
+  },
+  {
+    id: 5,
+    image: football,
+    title: "The club in my heart",
+    paragraph:
+      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+    link: "https://www.manutd.com/",
+  },
+  {
+    id: 6,
+    image: badminton,
+    title: "The sport that I play the best",
+    paragraph:
+      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+    link: "https://www.youtube.com/c/bwftv/videos",
+  },
+  {
+    id: 7,
+    image: chess,
+    title: "The website to contain my data",
+    paragraph:
+      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+    link: "https://players.chessbase.com/en/player/Hoang_Thai%20Bang/605991",
+  },
+];
 
 export default Hobbies;
--- src/Pages/HobbiesPage.js
+++ src/Pages/HobbiesPage.js
@@ -1,11 +1,28 @@
+import Title from "../Components/AboutContactTitle";
 import React from "react";
+import Hobbies from "../Components/Hobbies";
 
 function HobbiesPage() {
   return (
-    <div>
-      <div>
-        <div></div>
-      </div>
+    <div className="Hobbies">
+      <Title title={"Hobbies"} span={"Hobbies"} />
+      {Hobbies.map((hobby) => {
+        return (
+          <ul className="hobby" key={hobby.id}>
+            <li className="hobby-content">
+              <a
+                href={hobby.link}
+                className="linkWeb"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={hobby.image} alt="" />;
+              </a>
+              <p className="para">{hobby.paragraph}</p>
+            </li>
+          </ul>
+        );
+      })}
     </div>
   );
 }
