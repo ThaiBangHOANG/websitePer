47997f75683616b58471ad60fd6627b7bdba51b5
TB <thaibang1983hn@gmail.com>
2021-06-29 16:25:02 +0200

    update hobbies

--- src/Components/Hobbies.js
+++ src/Components/Hobbies.js
@@ -12,7 +12,7 @@ const Hobbies = [
     image: codewars,
     title: "The website to learn code",
     paragraph:
-      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum  Lorem ipsum Lorem ipsum Lorem ",
+      "From the time I have known about Codewars, I have been addicted by this website. Codewars is not just a website to study and enhance your coding skills. Moreover, friendly environment to find out and discuss the code with others.",
     link: "https://www.codewars.com/users/quanaotat",
   },
   {
@@ -20,7 +20,7 @@ const Hobbies = [
     image: sciencefiction,
     title: "The novel of science-fiction",
     paragraph:
-      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+      "The science-fiction novel that one of the most favorite types of books that I like to read the best which makes me feel relaxed and frees myself from all the pressure and fatigue of working all day long. ",
     link: "https://miscifi.com",
   },
   {
@@ -28,7 +28,7 @@ const Hobbies = [
     image: voyages,
     title: "The trip to Maroc",
     paragraph:
-      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+      "Travel, in my opinion, is one of the most exciting hobbies that most people all over the world like to do. Up to now, I have traveled to more than 30 countries. Let take a look at some of the pictures from my trips.",
     link: "https://photos.google.com/sharing",
   },
   {
@@ -36,7 +36,7 @@ const Hobbies = [
     image: music,
     title: "The music that I hear everyday",
     paragraph:
-      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+      "Let's music in your soul! YES, music is needed for our life! For me, music is not only a hobby. Music has motivated me, helps me to pass the moment when I fell down, keeps me on track, and always moves forward!",
     link: "https://www.youtube.com/channel/UC3Ab4R2NthIyqLB0Kt3xC1g/videos",
   },
   {
@@ -44,7 +44,7 @@ const Hobbies = [
     image: football,
     title: "The club in my heart",
     paragraph:
-      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+      "King of sport? The easy answer is FOOTBALL. I love this sport, and I love the Manchester United Club. This passion has followed me from the time that I was a child. Cheer up with me about this hobby.",
     link: "https://www.manutd.com/",
   },
   {
@@ -52,7 +52,7 @@ const Hobbies = [
     image: badminton,
     title: "The sport that I play the best",
     paragraph:
-      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+      "Which sport do I play the best? I will give you the answer is Badminton. WHY? Because I won the badminton championship at the Université de Limoges in singles and doubles for two years.",
     link: "https://www.youtube.com/c/bwftv/videos",
   },
   {
@@ -60,7 +60,7 @@ const Hobbies = [
     image: chess,
     title: "The website to contain my data",
     paragraph:
-      "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+      "CHESS - sport does not require much strengthening physique, but you can train your brain a lot. I am fortunate to join another chess club and attend various competitions during my stay in France. If you and I have the same hobby, it's my pleasure to play chess with you.",
     link: "https://players.chessbase.com/en/player/Hoang_Thai%20Bang/605991",
   },
 ];
